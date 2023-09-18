036c311e35a2ff96ebcc106af02c72d9750d4422
James <jamesszzhong@gmail.com>
2023-09-18 12:09:27 +1200

    added a button that does nothing

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QPushButton
 from PyQt5.QtCore import Qt
 
 app = QApplication(sys.argv)
@@ -11,23 +11,25 @@ window.setGeometry(100, 100, 1250, 750)
 style = """
     QWidget {
         background-color: #333;
+        border-radius: 10px;
+        color: white;
     }
     QLabel {
-        color: #fff;
         font-size: 16px;
         padding: 10px;
         background-color: #272822;
-        border-radius: 10px;
         text-align: center;
     }
     QLineEdit {
         background-color: #444;
-        color: #fff;
         font-size: 16px;
         border: 2px solid #555;
-        border-radius: 10px;
         padding: 8px;
         width: 25px;
+    }
+    QPushButton {
+        background-color: #1cde81;
+        border-radius: 5px;
     }
 """
 
@@ -35,19 +37,15 @@ window.setStyleSheet(style)
 
 layout = QGridLayout()
 
-title = QLabel("NCEA GPA Calculator")
-title.setStyleSheet(
-    "text-align: center;"+
-    "padding: 0;"
-)
-layout.addWidget(title, 0, 0, 1, 8)
-
 grades = [
     "Low Not Achieved", "Not Achieved", "High Not Achieved",
     "Low Achieved", "Achieved", "High Achieved",
     "Low Merit", "Merit", "High Merit",
     "Low Excellence", "Excellence", "High Excellence"
 ]
+
+title = QLabel("NCEA GPA Calulator")
+layout.addWidget(title)
 
 for row in range(3):
     for col in range(4):
@@ -58,6 +56,9 @@ for row in range(3):
 
         layout.addWidget(label, row + 1, col * 2)
         layout.addWidget(input_box, row + 1, col * 2 + 1)
+
+calculate = QPushButton("Calculate")
+layout.addWidget(calculate)
 
 window.setLayout(layout)
 window.show()
