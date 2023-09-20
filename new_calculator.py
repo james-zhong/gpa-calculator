7e7e1b928e2b16f067d668135009788e419e5d6c
James <jamesszzhong@gmail.com>
2023-09-20 22:34:46 +1200

    began to add grade input

--- new_calculator.py
+++ new_calculator.py
@@ -1,21 +1,34 @@
 import sys
-import typing
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 
 style = """
-    QWidget {
+    * {
         background-color: #272822;
-        color: white;
+        color: #f8f8f2;
+        font-family: Verdana;
     }
 
 
     QLabel#title {
         text-align: center;
         font-size: 20pt;
+        border: 3px solid #2cde85;
+        border-radius: 20px;
+    }
+    
+    QLabel#test {
+        border: 3px solid white;
     }
 
 """
+
+grades = [
+    "Low Not Achieved", "Not Achieved", "High Not Achieved",
+    "Low Achieved", "Achieved", "High Achieved",
+    "Low Merit", "Merit", "High Merit",
+    "Low Excellence", "Excellence", "High Excellence"
+]
 
 class Calculator(QMainWindow):
     def __init__(self):
@@ -28,20 +41,33 @@ class Calculator(QMainWindow):
         
         self.initUI()
     
+    # Function for creating a grade input and its respective label
+    def createGradeInputs(self, grade):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText(grade)
+        self.header.setAlignment(QtCore.Qt.AlignCenter)
+    
     # Create and add widgets  
     def initUI(self):
         # Title
         self.header = QtWidgets.QLabel(self, objectName="title")
         self.header.setText("NCEA GPA Calculator")
         self.header.setAlignment(QtCore.Qt.AlignCenter)
-        self.header.resize(1250, 50)
-        self.header.move(0, 15)
+        self.header.resize(700, 50) # Resize so the text fits
+        self.header.move(275, 25) # Move the title to the middle of the screen
+        
+        self.label = QtWidgets.QLabel(self, objectName="test")
+        self.label.setText("HI")
+        self.label.resize(100, 300)
+        self.label.move(0, 100)
 
 def window():
     app = QApplication(sys.argv)
+    app.setStyle("Fusion")
     win = Calculator()
     win.show() # Show all the widgets applied to the window
     sys.exit(app.exec_()) # Close application when the X is pressed
     
-if __name__ == "__main__": # Run if ran from a script and not a library
+# Run if ran from a script and not a library
+if __name__ == "__main__":
     window()
