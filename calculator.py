e3c64c5ac4c8a1bd47a48448e4ad0d95dc454bf7
James <jamesszzhong@gmail.com>
2023-09-22 22:54:30 +1200

    deleted the old script
    and changed the new script's name to the old script's name

--- calculator.py
+++ calculator.py
@@ -1,41 +1,32 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QPushButton
-from PyQt5.QtCore import Qt
+import sys, re
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 
-app = QApplication(sys.argv)
-
-window = QWidget()
-window.setWindowTitle("NCEA GPA Calculator")
-window.setGeometry(100, 100, 1250, 750)
-
+# Stylesheet for application
 style = """
-    QWidget {
-        background-color: #333;
-        border-radius: 10px;
-        color: white;
-    }
-    QLabel {
-        font-size: 16px;
-        padding: 10px;
+    * {
         background-color: #272822;
+        color: #f8f8f2;
+        font-family: Verdana;
+    }
+
+
+    QLabel#title {
         text-align: center;
+        font-size: 20pt;
+        border: 3px solid #2cde85;
+        border-radius: 20px;
     }
-    QLineEdit {
-        background-color: #444;
-        font-size: 16px;
-        border: 2px solid #555;
-        padding: 8px;
-        width: 25px;
+    
+    QLabel#input_title {
     }
-    QPushButton {
-        background-color: #1cde81;
-        border-radius: 5px;
+    
+    QLineEdit#input_box {
+        border: 3px solid #2cde85;
+        border-radius: 10px;
     }
+
 """
-
-window.setStyleSheet(style)
-
-layout = QGridLayout()
 
 grades = [
     "Low Not Achieved", "Not Achieved", "High Not Achieved",
@@ -44,23 +35,94 @@ grades = [
     "Low Excellence", "Excellence", "High Excellence"
 ]
 
-title = QLabel("NCEA GPA Calulator")
-layout.addWidget(title)
+class Calculator(QMainWindow):
+    def __init__(self):
+        super(Calculator, self).__init__()
 
-for row in range(3):
-    for col in range(4):
-        index = row * 4 + col
-        grade = grades[index]
-        label = QLabel(grade)
-        input_box = QLineEdit()
+        # Define window properties
+        self.setFixedSize(1118, 750)
+        self.setWindowTitle("NCEA GPA Calculator")
+        self.setStyleSheet(style)
+        
+        # Define UI variables
+        
+        # Define where the coordinates of the first input label should be
+        self.startingLabel_X = 83
+        self.startingLabel_Y = 100
+        
+        # Define where the coordinates of the first input box should be
+        self.startingInput_X = self.startingLabel_X
+        self.startingInput_Y = self.startingLabel_Y + 25
+        
+        # Storage for input boxes so it can be accessed through dictionary
+        self.inputs = {}
+        
+        # Create all the widgets
+        self.initUI()
+    
+    # Function for creating a grade input and its respective label
+    def createGradeInputs(self, grade, row, column):
+        # Create input label
+        self.label = QtWidgets.QLabel(self, objectName="input_title")
+        self.label.setText(grade)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.resize(252, 25)
+        self.label.move(self.startingLabel_X + (row * 350), self.startingLabel_Y + (column * 90)) # Move according to the row and column
+        
+        # Create input box
+        self.input = QtWidgets.QLineEdit(self, objectName ="input_box")
+        self.input.setAlignment(QtCore.Qt.AlignCenter)
+        self.input.setText("0")
+        self.input.resize(252, 25)
+        self.input.move(self.startingInput_X + (row * 350), self.startingInput_Y + (column * 90))  # Adjust the vertical position as needed
+        
+        # Store input box in dictonary so it can be accessed inividually
+        self.inputs[grade] = self.input
+        
+        # Check when text gets changed in input box
+        self.input.textChanged.connect(self.onInputTextChanged)
+        
+    def onInputTextChanged(self):
+        sender = self.sender() # Widget that changed text
+        text = sender.text()
+        
+        # Ensure that only numbers can be typed
+        numeric_text = re.sub(r'[^0-9]', '', text)
+        sender.setText(numeric_text)
+        
+        # Set text to 0 if input box is empty
+        if text == "":
+            sender.setText("0")   
+        elif text[0] == "0" and text != "0": # Remove the "0" in front of number when not empty
+            sender.setText(text.lstrip("0"))
+        
+    # Create and add widgets  
+    def initUI(self):
+        # Title
+        self.header = QtWidgets.QLabel(self, objectName="title")
+        self.header.setText("NCEA GPA Calculator")
+        self.header.setAlignment(QtCore.Qt.AlignCenter)
+        self.header.resize(700, 50) # Resize so the text fits
+        self.header.move(209, 25) # Move the title to the middle of the screen
+        
+        # Input boxes
+        rows = 3
+        columns = 4
+        
+        for row in range(rows):
+            for column in range(columns):
+                index = row * 4 + column
+                grade = grades[index]
+                
+                self.createGradeInputs(grade, row, column)
 
-        layout.addWidget(label, row + 1, col * 2)
-        layout.addWidget(input_box, row + 1, col * 2 + 1)
-
-calculate = QPushButton("Calculate")
-layout.addWidget(calculate)
-
-window.setLayout(layout)
-window.show()
-
-sys.exit(app.exec_())
+def window():
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+    win = Calculator()
+    win.show() # Show all the widgets applied to the window
+    sys.exit(app.exec_()) # Close application when the X is pressed
+    
+# Run if ran from a script and not a library
+if __name__ == "__main__":
+    window()
