dfc1ec9989130099df225697012f173f2aad0bd5
James <jamesszzhong@gmail.com>
2023-10-04 22:17:12 +1300

    User manual
    -finished it
    -added a new script that just stores all the text for the user manual because it looks messy. is there a bettre way to do this?

--- help.py
+++ help.py
@@ -1,10 +1,35 @@
-import sys, ctypes, time, help_text
+import ctypes, time, help_text
 from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QScrollArea
-from PyQt5.QtGui import QCursor
+from PyQt5.QtWidgets import QMainWindow
+
+# UI for different pages
+def page1(self):
+    self.page_title.setText("<u>Overview</u>")
+    self.page_desc.setText(help_text.overview_desc)
+    
+def page2(self):
+    self.page_title.setText("<u>Discretion</u>")
+    self.page_desc.setText(help_text.warning_desc)
+
+def page3(self):
+    self.page_title.setText("<u>Usage</u>")
+    self.page_desc.setText(help_text.ui_desc)
+
+def page4(self):
+    self.page_title.setText("<u>Credits</u>")
+    self.page_desc.setText(help_text.credits_desc)
 
 # Track the pages of user manual
 current_page = 1
+total_pages = 4
+
+# Dictionary to map current page number
+page_functions = {
+    1: page1,
+    2: page2,
+    3: page3,
+    4: page4
+}
 
 class Manual(QMainWindow):
     def __init__(self):
@@ -31,6 +56,9 @@ class Manual(QMainWindow):
         # Draw all widgets
         self.initUI()
         
+        # Global variable for the page display QLabel
+        self.page_count_display = None
+        
     def initUI(self):
         # Title
         self.header = QtWidgets.QLabel(self, objectName="header")
@@ -40,49 +68,48 @@ class Manual(QMainWindow):
         
         # Page navigation
         self.page_display = QtWidgets.QLabel(self, objectName="page_display")
-        self.page_display.setText("0 / 10")
+        self.page_display.setText(f"{current_page} / {total_pages}")
         self.page_display.setAlignment(QtCore.Qt.AlignCenter)
         self.page_display.resize(200, 50)
         self.page_display.move(275, 500)
+        self.page_count_display = self.page_display
         
         self.go_back = QtWidgets.QPushButton(self, objectName="turn_button")
         self.go_back.setText("<")
         self.go_back.setFlat(True)
+        self.go_back.clicked.connect(lambda: self.changePage("back"))
         self.go_back.resize(50, 50)
         self.go_back.move(225, 500)
         
         self.go_next = QtWidgets.QPushButton(self, objectName="turn_button")
         self.go_next.setText(">")
         self.go_next.setFlat(True)
+        self.go_next.clicked.connect(lambda: self.changePage("next"))
         self.go_next.resize(50, 50)
         self.go_next.move(475, 500)
         
-        # Different pages
-        def page1(self):
-            # Description of the app
-            self.overview_title = QtWidgets.QLabel(self, objectName="section_title")
-            self.overview_title.setText("<u>Overview</u>")
-            self.overview_title.setAlignment(QtCore.Qt.AlignCenter)
-            self.overview_title.resize(175, 50)
-            self.overview_title.move(5, 100)
-            
-            self.overview_desc = QtWidgets.QLabel(self, objectName="section_desc")
-            self.overview_desc.setText(help_text.overview_desc)
-            self.overview_desc.setWordWrap(True)
-            self.overview_desc.resize(700, 350)
-            self.overview_desc.move(25, 140)
-            
-        page1() # Temp
-"""
-Temporary so I can test out this window quicker
-"""
-
-if __name__ == "__main__":
-    def window():
-        app = QApplication(sys.argv)
-        app.setStyle("Fusion")
-        win = Manual()
-        win.show() # Show all the widgets applied to the window
-        sys.exit(app.exec_()) # Close application when the X is pressed
+        # Page set up
+        self.page_title = QtWidgets.QLabel(self, objectName="section_title")
+        self.page_title.setAlignment(QtCore.Qt.AlignCenter)
+        self.page_title.resize(175, 50)
+        self.page_title.move(5, 100)
         
-    window()
+        self.page_desc = QtWidgets.QLabel(self, objectName="section_desc")
+        self.page_desc.setWordWrap(True)
+        self.page_desc.resize(700, 350)
+        self.page_desc.move(25, 140)
+        
+        # Change the UI to the respective UI on current page
+        page_functions[current_page](self)
+
+    def changePage(self, turn):
+        global current_page, total_pages
+        
+        # Change the current page
+        if turn == "back":
+            current_page =  max(1, current_page - 1)
+        elif turn == "next":
+            current_page = min(total_pages, current_page + 1)
+            
+        self.page_display.setText(f"{current_page} / {total_pages}")
+        page_functions[current_page](self)
--- help_text.py
+++ help_text.py
@@ -1,15 +1,17 @@
 # The lines breaks were so that it fit on my screen
 
-overview_desc = """
-This application will convert all of your NCEA grades into a grade point average (GPA). 
-The GPA is a 12 point scale, where 1 is the lowest (Low Not Achieved) and 12 is the 
-highest (High Excellence). You can input how many grades you have gotten (from your 
-school webportal, etc) into the provided input boxes and the GPA will be calculated 
-automatically. Once there is input, it will also show you how many Excellences and 
-High Excellences you need to raise your GPA to an Excellence and also show you how 
-many High Excellences you need to raise your GPA to a High Excellence (if your 
-GPA is not already at Excellence/High Excellence). If you would like to save your 
-inputs for next time, you can save your inputs and also load it using the Save/Load 
-buttons. Please do not attempt to change the properties in the save file or replace 
-it, as it may break the program and/or potentially harm your own device. 
-""".replace("\n", "")
+overview_desc = "This application will convert all of your NCEA grades into a grade point average (GPA). The GPA is a 12 point scale, where 1 is the lowest (Low Not Achieved) and 12 is the highest (High Excellence). You can input how many grades you have gotten (from your school webportal, etc) into the provided input boxes and the GPA will be calculated automatically. Once there is input, it will also show you how many Excellences and High Excellences you need to raise your GPA to an Excellence and also show you how many High Excellences you need to raise your GPA to a High Excellence (if your GPA is not already at Excellence/High Excellence). If you would like to save your inputs for next time, you can save your inputs and also load it using the Save/Load buttons."
+
+warning_desc = """
+Please note that this GPA is not accurate, as grades received from certain subjects may not be applicable for Low/High grades, etc. This application is so that you know where you're at and how you can improve.
+
+If you plan on using the save and load feature, the save file will be named as 'save.vault'. Please do not replace this file with another random file and name it 'save.vault'. When loading data, the program will unpack the data from save.vault. If you modify the data in save.vault, there may potentially be an error when trying to load the data in. If you replace the save.vault file with a random file from the internet and rename it to 'save. vault' (for some reason), you can potentially get a virus because you don't know what is on the file.
+"""
+
+ui_desc = """
+To input your grades, using tha amount of Excellences as an example, firstly, go to your student webportal (or equivalent) to see an overview of your results and count how many Excellences you have. Next, go to the box underneath the 'Excellence' label and click on the box and enter the amount of Excellences you have. Repeat this with all the grades. If you do not have any results of a particular grade, it will stay at 0.
+
+Press the buttons at the top-left to save/load your inputs. A message will appear to confirm the success/error. Saving will store all inputs and can be loaded anytime, but only one save file can be saved at a time, so saving overrides previous saved data. Multiple save files may be added in the future.
+"""
+
+credits_desc = "James"
